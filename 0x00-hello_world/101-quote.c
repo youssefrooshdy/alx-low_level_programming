938facf6934ce3d4c9d7d8f5afd78eee2acaad72
Youssef roshdy <2101033@eng.asu.edu.eg>
2023-07-14 01:38:26 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc -S -masm=intel  $CFILE -o `echo $CFILE | cut -d. -f1`.s
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ * write (int fd,const void *buf,size_t count)
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
