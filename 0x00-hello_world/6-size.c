e6036d301bab0125345bd3b01e754dad1eb85bbd
Youssef roshdy <2101033@eng.asu.edu.eg>
2023-07-14 00:50:04 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point 
  *
- * Description: using sizeof to print the size of vatious types.
+ * Description: using sizeof to print the size of various types.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
